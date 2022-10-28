82c529916f250e9d61a2cf1a42c552d9d097d91a
Vasilliy <Vasily.vvk@gmail.com>
2022-10-28 20:03:30 +0300

    Done

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        String result =
+                (x > 0 && y > 0) ? "first" :
+                        (x < 0 && y > 0) ? "second" :
+                                (x < 0 && y < 0) ? "third" :
+                                        (x > 0 && y < 0) ? "fourth" :
+                                                "zero";
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,41 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (month == 1) {
+            System.out.println(31);
+        }
+        if (month == 2 && (year == 2028 || year == 2024 || year == 2020 || year == 2016 || year ==
+                2012 || year == 2008 || year == 2004 || year == 2000 || year == 1996 || year ==
+                1992 || year == 1988 || year == 1984 || year == 1980 || year == 1976 || year ==
+                1972 || year == 1968 || year == 1964 || year == 1960 || year == 1956 || year == 1952 || year == 1948
+                || year == 1944 || year == 1940 || year == 1936 || year == 1932 || year == 1928 || year ==
+                1924 || year == 1920 || year == 1916 || year == 1912 || year == 1908 || year == 1904)) {
+            System.out.println(29);
+            return;
+        } else if (month == 2) {
+            System.out.println(28);
+        } else if (month == 3) {
+            System.out.println(31);
+        } else if (month == 4) {
+            System.out.println(30);
+        } else if (month == 5) {
+            System.out.println(31);
+        } else if (month == 6) {
+            System.out.println(30);
+        } else if (month == 7) {
+            System.out.println(31);
+        } else if (month == 8) {
+            System.out.println(31);
+        } else if (month == 9) {
+            System.out.println(30);
+        } else if (month == 10) {
+            System.out.println(31);
+        } else if (month == 11) {
+            System.out.println(30);
+        } else if (month == 12) {
+            System.out.println(31);
+        } else {
+            System.out.println("invalid date");
+        }
     }
 }
